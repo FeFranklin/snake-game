27e45c8ee0126c48ce634c37198b6093bb55cb8a
Felipe Franklin <felipelfbezerra54@gmail.com>
2020-08-20 18:21:46 +0200

    Updates

---
+++ precache-manifest.ec048b678033de8d34006e92657d81a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ea3839d5b35effb0acf2614b498c50",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "3637997c1ce22685a250",
+    "url": "/snake-game/static/css/main.57b2e633.chunk.css"
+  },
+  {
+    "revision": "96df81dec36b3c9e4a58",
+    "url": "/snake-game/static/js/2.88ed65ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/snake-game/static/js/2.88ed65ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3637997c1ce22685a250",
+    "url": "/snake-game/static/js/main.eeb36db9.chunk.js"
+  },
+  {
+    "revision": "18005e22df855f84fc12",
+    "url": "/snake-game/static/js/runtime-main.f9e1bf59.js"
+  }
+]);
